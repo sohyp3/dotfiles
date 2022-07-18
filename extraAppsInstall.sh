3993def44245f59762cfc741e155710df3ebb0d5
sohyp3 <0xshb@protonmail.com>
2022-07-18 14:51:15 +0300

    first

---
+++ extraAppsInstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+flatpak install com.bitwarden.desktop -y && flatpak install com.github.marktext.marktext -y  && flatpak install com.obsproject.Studio -y && flatpak install org.qbittorrent.qBittorrent -y && flatpak install fr.handbrake.ghb -y && flatpak install org.kde.Kdenlive -y && flatpak install org.gimp.GIMP
